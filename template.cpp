35114860e3ef5646d7d5ae4d12b3033ae5c4a81e
kei <clavis1107@gmail.com>
2017-10-15 08:10:07 +0100

    add template

---
+++ template.cpp
@@ -0,0 +1,12 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define INF 1<<30
+#define LINF 1LL<<60
+
+int main(void) {
+	cin.tie(0); ios::sync_with_stdio(false);
+	return 0;
+}

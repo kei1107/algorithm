8d0e978e6913be17b92a6f0742769be379275109
kei1107 <clavis1107@gmail.com>
2017-10-24 20:52:45 +0900

    add template and code

---
+++ CGL/Segments:Lines/Parallel_Orthogonal.cpp
@@ -0,0 +1,66 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define INF 1<<30
+#define LINF 1LL<<60
+
+typedef long double ld;
+typedef complex<ld> Point;
+const ld eps = 1e-9, pi = acos(-1.0);
+namespace std {
+    bool operator<(const Point &lhs, const Point &rhs) {
+        if (lhs.real() < rhs.real() - eps) return true;
+        if (lhs.real() > rhs.real() + eps) return false;
+        return lhs.imag() < rhs.imag();
+    }
+}
+Point input_point() {ld x, y; cin >> x >> y; return Point(x, y);} // 点の入力
+bool eq(ld a, ld b) {return (abs(a - b) < eps);} // 誤差つき等号判定
+ld dot(Point a, Point b) {return real(conj(a) * b);} // 内積
+ld cross(Point a, Point b) {return imag(conj(a) * b);} // 外積
+
+// 直線の定義
+class Line {
+public:
+    Point a, b;
+    Line() : a(Point(0, 0)), b(Point(0, 0)) {}
+    Line(Point a, Point b) : a(a), b(b) {}
+    Point operator[](const int _num) {
+        if (_num == 0)return a;
+        else if (_num == 1)return b;
+        else assert(false);
+    }
+};
+
+// 円の定義
+class Circle {
+public:
+    Point p;
+    ld r;
+    Circle() : p(Point(0, 0)), r(0) {}
+    Circle(Point p, ld r) : p(p), r(r) {}
+};
+
+/*
+ 平行・垂直判定
+ 平行：2 垂直：1 otherwise：0
+ */
+int main(void) {
+    cin.tie(0); ios::sync_with_stdio(false);
+    int q; cin >> q;
+    for(int i = 0; i < q;i++){
+        Point p0 = input_point();
+        Point p1 = input_point();
+        Point p2 = input_point();
+        Point p3 = input_point();
+        
+        Point s1 = p1 - p0, s2 = p3 - p2;
+        if(abs(cross(s1,s2)) < eps) cout << 2 << endl;
+        else if(abs(dot(s1,s2)) < eps) cout << 1 << endl;
+        else cout << 0 << endl;
+    }
+    return 0;
+}
+

5237a0afc0d45e63508f65c4dbc21f15a16503e4
kei <clavis1107@gmail.com>
2017-10-25 12:00:26 +0100

    凸包の修正

--- template/template(Geometry_base)ver4.cpp
+++ template/template(Geometry_base)ver4.cpp
@@ -155,24 +155,39 @@ int is_in_polygon(const Polygon &poly, Point p) {
     }
     return eq(angle, 0) ? 0 : 2;
 }
-// 凸包
+// 凸包 : 凸多角形のある一辺上にある点も含む
 Polygon convex_hull(vector<Point> ps) {
-    int n = (int)ps.size();
-    if(n < 3) return ps;
-    sort(ps.begin(), ps.end());
-    Polygon u = {ps[0], ps[1]},l = {ps[n-1],ps[n-2]};
-    for(int i = 2; i < n; i++){
-        for(int j = (int)u.size(); j>=2 && ccw(u[j-2],u[j-1],ps[i])>=0;j--)u.pop_back();
-        u.push_back(ps[i]);
-    }
-    for(int i = n-3;i>=0;i--){
-        for(int j = (int)l.size(); j>=2 && ccw(l[j-2],l[j-1],ps[i])>=0;j--)l.pop_back();
-        l.push_back(ps[i]);
-    }
-    reverse(l.begin(),l.end());
-    for(int i=(int)u.size()-2; i>= 1; i--)l.push_back(u[i]);
-    return l;
+	int n = (int)ps.size();
+	if (n < 3) return ps;
+	sort(ps.begin(), ps.end());
+	Polygon u = { ps[0], ps[1] }, l = { ps[n - 1],ps[n - 2] };
+	for (int i = 2; i < n; i++) {
+		for (int j = (int)u.size(); j >= 2 && ccw(u[j - 2], u[j - 1], ps[i]) >= 0;j--)u.pop_back();
+		u.push_back(ps[i]);
+	}
+	for (int i = n - 3;i >= 0;i--) {
+		for (int j = (int)l.size(); j >= 2 && ccw(l[j - 2], l[j - 1], ps[i]) >= 0;j--)l.pop_back();
+		l.push_back(ps[i]);
+	}
+	reverse(l.begin(), l.end());
+	for (int i = (int)u.size() - 2; i >= 1; i--)l.push_back(u[i]);
+	return l;
 }
+// 凸包 : 凸多角形のある一辺上にある点を含まない
+/*
+Polygon convex_hull(vector<Point> ps) {
+	int n = ps.size();
+	int k = 0;
+	sort(ps.begin(), ps.end());
+	Polygon ch(2 * n);
+	for (int i = 0; i < n; ch[k++] = ps[i++])
+		while (k >= 2 && ccw(ch[k - 2], ch[k - 1], ps[i]) <= 0) --k;
+	for (int i = n - 2, t = k + 1; i >= 0; ch[k++] = ps[i--])
+		while (k >= t && ccw(ch[k - 2], ch[k - 1], ps[i]) <= 0) --k;
+	ch.resize(k - 1);
+	return ch;
+}
+*/
 // 凸多角形の直径
 ld convex_diameter(const Polygon& poly){
     int n = (int)poly.size();

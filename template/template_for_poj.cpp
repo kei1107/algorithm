957341ffc49a59cd4b6e2171a1d97306aef39b58
kei1107 <clavis1107@gmail.com>
2017-12-27 16:22:32 +0900

    template for poj

---
+++ template/template_for_poj.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <climits>
+#include <cfloat>
+#include <map>
+#include <utility>
+#include <set>
+#include <bitset>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include <list>
+#include <numeric>
+#include <algorithm>
+#include <functional>
+#include <sstream>
+#include <complex>
+#include <stack>
+#include <queue>
+#include <deque>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define INF 1<<30
+#define LINF 1LL<<60
+
+/*
+<url:>
+問題文============================================================
+
+=================================================================
+
+解説=============================================================
+
+================================================================
+*/
+int main(void) {
+	cin.tie(0); ios::sync_with_stdio(false);
+	return 0;
+}

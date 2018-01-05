98e18aa19b039284e0e96c2d7c4b7ad71c99a7a8
kei1107 <clavis1107@gmail.com>
2018-01-05 20:01:53 +0900

    Lowlink-ver2-

---
+++ GRL/Connected_Components/Lowlink-ver2-.cpp
@@ -0,0 +1,73 @@
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> G[1000000];
+vector<pair<int, int> > bridge;
+vector<int> articulation;
+int ord[1000000], low[1000000];
+bool vis[1000000];
+
+void dfs(int v, int p, int &k)
+{
+    vis[v] = true;
+    
+    ord[v] = k++;
+    low[v] = ord[v];
+    
+    bool isArticulation = false;
+    int ct = 0;
+    
+    for (int i = 0; i < G[v].size(); i++){
+        if (!vis[G[v][i]]){
+            ct++;
+            dfs(G[v][i], v, k);
+            low[v] = min(low[v], low[G[v][i]]);
+            if (~p && ord[v] <= low[G[v][i]]) isArticulation = true;
+            if (ord[v] < low[G[v][i]]) bridge.push_back(make_pair(min(v, G[v][i]), max(v, G[v][i])));
+        }
+        else if (G[v][i] != p){
+            low[v] = min(low[v], ord[G[v][i]]);
+        }
+    }
+    
+    if (p == -1 && ct > 1) isArticulation = true;
+    if (isArticulation) articulation.push_back(v);
+}
+
+int main()
+{
+    int n, m;
+    
+    scanf("%d %d", &n, &m);
+    
+    for (int i = 0; i < m; i++){
+        int x, y;
+        scanf("%d %d", &x, &y);
+        G[--x].push_back(--y);
+        G[y].push_back(x);
+    }
+    
+    int k = 0;
+    for (int i = 0; i < n; i++){
+        if (!vis[i]) dfs(i, -1, k);
+    }
+    
+    sort(bridge.begin(), bridge.end());
+    sort(articulation.begin(), articulation.end());
+    
+    printf("%d\n", articulation.size());
+    
+    for (int i = 0; i < articulation.size(); i++)
+        printf("%d\n", articulation[i] + 1);
+    
+    printf("%d\n", bridge.size());
+    
+    for (int i = 0; i < bridge.size(); i++)
+        printf("%d %d\n", bridge[i].first + 1, bridge[i].second + 1);
+    
+    
+    return (0);
+}

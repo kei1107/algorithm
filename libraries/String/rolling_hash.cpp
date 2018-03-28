4b1f1eb7790a264016b522e54131d4dd11cf2bfd
kei1107 <clavis1107@gmail.com>
2018-03-28 16:14:33 +0900

    rolling_hash

--- libraries/String/rolling_hash.cpp
+++ libraries/String/rolling_hash.cpp
@@ -1,37 +1,10 @@
-#include "bits/stdc++.h"
-using namespace std;
-// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2444&lang=jp
-typedef long long ll;
-typedef unsigned long long ull;
-const ull B = 100000007;
-
-int main() {
-    cin.tie(0); ios::sync_with_stdio(false);
-    int n, m; cin >> n >> m;
-    string s; cin >> s;
-    vector<ull> H(n + 1,0), b(n + 1, 1);
-    set<ull> Ans;
-    /*
-     rolling-hash algorithm
-     initialize
-     b[n] := B^n
-     H[n] := s[0]*b^(n-1) + s[1]*b^(n-2) +... +s[n-1]*b^0
-     */
-    for (int i = 0; i < n;i++) {
-        b[i + 1] = b[i] * B;
-        H[i + 1] = H[i] * B + s[i];
+struct RH {
+    vector<unsigned long long> p,hash;
+    void init(const string s) {
+        unsigned long long m=1000000007,q=1,n=s.size();
+        p.resize(n+1);hash.resize(n+1);p[0]=q;hash[0]=0;
+        for(int i=0;i<n;i++){p[i+1]=p[i]*m;hash[i+1]=s[i]+hash[i]*m;}
     }
-    
-    int l, r; l = r = 0;
-    for (int k = 0; k < m;k++) {
-        string q; cin >> q;
-        if (q == "L++") l++;
-        else if (q == "L--") l--;
-        else if (q == "R++") r++;
-        else r--;
-        ull _hash = H[r + 1] - H[l] * b[r - l + 1];
-        Ans.insert(_hash);
-    }
-    cout << Ans.size() << endl;
-    return 0;
-}
+    unsigned long long h(int i)const{return hash[i];}
+    unsigned long long h(int i,int j)const{return h(j)-h(i)*p[j-i];}
+};

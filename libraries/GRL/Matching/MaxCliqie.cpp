cb6e62793da3accd880393acc8846e45f07360e7
kei1107 <clavis1107@gmail.com>
2018-06-17 10:22:28 +0900

    最大クリーク

---
+++ libraries/GRL/Matching/MaxCliqie.cpp
@@ -0,0 +1,101 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+const int INF = 1e9;
+const ll LINF = 1e18;
+template<class S,class T> ostream& operator << (ostream& out,const pair<S,T>& o){ out << "(" << o.first << "," << o.second << ")"; return out; }
+template<class T> ostream& operator << (ostream& out,const vector<T> V){ for(int i = 0; i < V.size(); i++){ out << V[i]; if(i!=V.size()-1) out << " ";} return out; }
+template<class T> ostream& operator << (ostream& out,const vector<vector<T> > Mat){ for(int i = 0; i < Mat.size(); i++) { if(i != 0) out << endl; out << Mat[i];} return out; }
+template<class S,class T> ostream& operator << (ostream& out,const map<S,T> mp){ out << "{ "; for(auto it = mp.begin(); it != mp.end(); it++){ out << it->first << ":" << it->second; if(mp.size()-1 != distance(mp.begin(),it)) out << ", "; } out << " }"; return out; }
+
+
+// 最大クリーク問題
+// あるグラフにおいて完全グラフとなるような最大の部分グラフの大きさを求める
+// 補グラフでの最大安定集合問題と同値となる
+
+const int MAXN = 111;
+struct MaxClique { // 0-base
+    typedef bitset<MAXN> Int;
+    Int linkto[MAXN], v[MAXN];
+    int n;
+    MaxClique(int n = 0){init(n);}
+    void init(int _n) {
+        n = _n;
+        for (int i = 0; i < n; i++) {
+            linkto[i].reset();
+            v[i].reset();
+        }
+    }
+    void addEdge(int a, int b) { v[a][b] = v[b][a] = 1; }
+    int popcount(const Int& val){ return (int)val.count(); }
+    int lowbit(const Int& val){
+        int ret = 0; while(!val[ret]){ret++; if(ret >= MAXN) break;} return ret;
+        //return val._Find_first();
+    }
+    int ans, stk[MAXN];
+    int id[MAXN], di[MAXN], deg[MAXN];
+#undef N
+    Int cans;
+    void maxclique(int elem_num, Int candi) {
+        if (elem_num > ans) {
+            ans = elem_num;
+            cans.reset();
+            for (int i = 0; i < elem_num; i++)
+                cans[id[stk[i]]] = 1;
+        }
+        int potential = elem_num + popcount(candi);
+        if (potential <= ans) return;
+        int pivot = lowbit(candi);
+        Int smaller_candi = candi & (~linkto[pivot]);
+        while (smaller_candi.count() && potential > ans) {
+            int next = lowbit(smaller_candi);
+            candi[next] = !candi[next];
+            smaller_candi[next] = !smaller_candi[next];
+            potential--;
+            if (next == pivot || (smaller_candi & linkto[next]).count()) {
+                stk[elem_num] = next;
+                maxclique(elem_num + 1, candi & linkto[next]);
+            }
+        }
+    }
+    int solve() {
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+            deg[i] = (int)v[i].count();
+        }
+        sort(id, id + n, [&](int id1, int id2) {
+            return deg[id1] > deg[id2]; });
+        for (int i = 0; i < n; i++)
+            di[id[i]] = i;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (v[i][j])
+                    linkto[di[i]][di[j]] = 1;
+        Int cand; cand.reset();
+        for (int i = 0; i < n; i++)
+            cand[i] = 1;
+        ans = 1;
+        cans.reset(); cans[0] = 1;
+        maxclique(0, cand);
+        return ans;
+    }
+};
+
+void solve_abc_002_d() {
+    int n, e; cin >> n >> e;
+    MaxClique solver(n);
+   // solver.init(n);
+    for(int i = 0; i < e;i++){
+        int a, b; cin >> a >> b;
+        a--; b--;
+        solver.addEdge(a, b);
+    }
+    cout << solver.solve() << endl;
+}
+int main(void){
+    cin.tie(0); ios_base::sync_with_stdio(false);
+    solve_abc_002_d();
+    return 0;
+}

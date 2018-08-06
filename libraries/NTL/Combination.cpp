43eac1f46edba973b1f4950216e64f2bc636b012
kei1107 <clavis1107@gmail.com>
2018-08-07 00:52:03 +0900

    Combination

--- libraries/NTL/Combination.cpp
+++ libraries/NTL/Combination.cpp
@@ -12,7 +12,7 @@ for (int i = 0; i <= MAX_N; i++) {
 ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 
 
-const ll MAX_N = 2e5 + 10;
+const ll MAX_N = 3e6 + 10;
 const ll MOD = 1e9+7;
 ll Inv[MAX_N];
 ll fact[MAX_N];
@@ -35,16 +35,27 @@ void fact_init(){
 ll powmod(ll a,ll b) {ll res=1;a%=MOD; for(;b;b>>=1){if(b&1)res=res*a%MOD;a=a*a%MOD;}return res;}
 // 逆元 1/a % MOD
 ll inversemod(ll a) { return powmod(a,MOD - 2);}
+
+// 1 以上 N 以下の N 個の整数の中から，相異なる K 個の整数を選ぶパターンの数
 ll nCr(int n,int r) {
     if(n < r) return 0;
     return (fact[n] * (factInv[r] * factInv[n-r] % MOD)) % MOD;
+}
+// 1 以上 N 以下の N 個の整数の中から，相異なる K 個の整数を選び，順番に並べるパターンの数
+ll nPr(int n,int r){
+    if(n < r) return 0;
+    return (fact[n]*factInv[n-r])%MOD;
+}
+// 1 以上 N 以下の N 個の整数の中から，重複を許して K 個の整数を選ぶパターンの数
+ll nHr(int n,int r){
+    if(n == 0 && r == 0) return 1;
+    return nCr(n+r-1,r);
 }
 
 void init(){
     Inv_init();
     fact_init();
 }
-
 ============================================================
 C(n,m) = C(n,m-1) * (n-m+1) / m.
 C(n,m) = C(n,n-m)

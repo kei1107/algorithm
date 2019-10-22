ee95e19d5e115107d3fe6d3ef274afae35a6fb44
kei1107 <clavis1107@gmail.com>
2019-10-22 15:40:52 +0900

    gauss_jordan update

--- libraries/NTL/gauss_jordan.cpp
+++ libraries/NTL/gauss_jordan.cpp
@@ -1,42 +1,234 @@
-#include "bits/stdc++.h"
-using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-const int INF = 1e9;
-const ll LINF = 1e18;
-template<class S,class T> ostream& operator << (ostream& out,const pair<S,T>& o){ out << "(" << o.first << "," << o.second << ")"; return out; }
-template<class T> ostream& operator << (ostream& out,const vector<T> V){ for(int i = 0; i < V.size(); i++){ out << V[i]; if(i!=V.size()-1) out << " ";} return out; }
-template<class T> ostream& operator << (ostream& out,const vector<vector<T> > Mat){ for(int i = 0; i < Mat.size(); i++) { if(i != 0) out << endl; out << Mat[i];} return out; }
-template<class S,class T> ostream& operator << (ostream& out,const map<S,T> mp){ out << "{ "; for(auto it = mp.begin(); it != mp.end(); it++){ out << it->first << ":" << it->second; if(mp.size()-1 != distance(mp.begin(),it)) out << ", "; } out << " }"; return out; }
-
 /*
- GAUSS JORDAN
+ * 参照：http://drken1215.hatenablog.com/entry/2019/03/20/202800
  */
-template<typename T>
-vector<T> gauss_jordan(const vector<vector<T>>& A, const vector<T>& b) {
-    const double EPS = 1e-9; int n = (int)A.size();
-    vector<vector<T>> B(n, vector<T>(n + 1));
-    for (int i = 0; i < n; i++) for (int j = 0; j < n; j++) B[i][j] = A[i][j];
-    for (int i = 0; i < n; i++) B[i][n] = b[i];
-    
-    for (int i = 0; i < n; i++) {
-        int pivot = i;
-        for (int j = i; j < n; j++) {if (abs(B[i][j]) > abs(B[pivot][i]))pivot = j; }
-        swap(B[i], B[pivot]);
-        
-        if (abs(B[i][i]) < EPS) { //解がないか一意でない
-            cerr << "error be." << endl; return vector<T>();
-        }
-        
-        for (int j = i + 1; j <= n; j++)B[i][j] /= B[i][i];
-        for (int j = 0; j < n; j++) {
-            if (i != j) {
-                for (int k = i + 1; k <= n; k++) B[j][k] -= B[j][i] * B[i][k];
+// ============================================================================================= //
+// ============================================================================================= //
+// ============================================================================================= //
+// ============================================================================================= //
+/*
+ * Gauss-Jordan(実数)
+ */
+
+using D = double;
+const D EPS = 1e-10;
+// matrix
+template<class T> struct Matrix {
+    vector<vector<T> > val;
+    Matrix(int n, int m, T x = 0) : val(n, vector<T>(m, x)) {}
+    void init(int n, int m, T x = 0) {val.assign(n, vector<T>(m, x));}
+    size_t size() const {return val.size();}
+    inline vector<T>& operator [] (int i) {return val[i];}
+};
+
+// A : m x nの行列
+// return : Aのrank
+template<class T> int GaussJordan(Matrix<T> &A, bool is_extended = false) {
+    int m = A.size(), n = A[0].size();
+    int rank = 0;
+    for (int col = 0; col < n; ++col) {
+        // 拡大係数行列の場合は最後の列は掃き出ししない
+        if (is_extended && col == n-1) break;
+        // ピボットを探す
+        int pivot = -1;
+        T ma = EPS;
+        for (int row = rank; row < m; ++row) {
+            if (abs(A[row][col]) > ma) {
+                ma = abs(A[row][col]);
+                pivot = row;
             }
         }
+        // ピボットがなかったら次の列へ
+        if (pivot == -1) continue;
+        // まずは行を swap
+        swap(A[pivot], A[rank]);
+        // ピボットの値を 1 にする
+        auto fac = A[rank][col];
+        for (int col2 = 0; col2 < n; ++col2) A[rank][col2] /= fac;
+        // ピボットのある列の値がすべて 0 になるように掃き出す
+        for (int row = 0; row < m; ++row) {
+            if (row != rank && abs(A[row][col]) > EPS) {
+                auto fac = A[row][col];
+                for (int col2 = 0; col2 < n; ++col2) {
+                    A[row][col2] -= A[rank][col2] * fac;
+                }
+            }
+        }
+        ++rank;
     }
-    vector<T> x(n);//解
-    for (int i = 0; i < n; i++) x[i] = B[i][n];
-    return x;//veci.
+    return rank;
 }
+
+// 入力：連立一次方程式 Ax = b
+// return : Ax = b の解
+template<class T> vector<T> linear_equation(Matrix<T> A, vector<T> b) {
+    // extended
+    int m = A.size(), n = A[0].size();
+    Matrix<T> M(m, n + 1);
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) M[i][j] = A[i][j];
+        M[i][n] = b[i];
+    }
+    int rank = GaussJordan(M, true);
+
+    // check if it has no solution
+    vector<T> res;
+    for (int row = rank; row < m; ++row) if (abs(M[row][n]) > EPS) return res;
+
+    // answer
+    res.assign(n, 0);
+    for (int i = 0; i < rank; ++i) res[i] = M[i][n];
+    return res;
+}
+
+// ============================================================================================= //
+// ============================================================================================= //
+// ============================================================================================= //
+// ============================================================================================= //
+/*
+ * Gauss-Jordan( mod. p / p is prime)
+ */
+
+// 逆元計算
+long long modinv(long long a, long long mod) {
+    long long b = mod, u = 1, v = 0;
+    while (b) {
+        long long t = a/b;
+        a -= t*b; swap(a, b);
+        u -= t*v; swap(u, v);
+    }
+    u %= mod;
+    if (u < 0) u += mod;
+    return u;
+}
+
+// matrix
+template<int MOD> struct Matrix {
+    vector<vector<long long> > val;
+    Matrix(int n, int m, long long x = 0) : val(n, vector<long long>(m, x)) {}
+    void init(int n, int m, long long x = 0) {val.assign(n, vector<long long>(m, x));}
+    size_t size() const {return val.size();}
+    inline vector<long long>& operator [] (int i) {return val[i];}
+};
+
+// A : m x nの行列
+// return : Aのrank
+template<int MOD> int GaussJordan(Matrix<MOD> &A, bool is_extended = false) {
+    int m = A.size(), n = A[0].size();
+    for (int row = 0; row < m; ++row)
+        for (int col = 0; col < n; ++col)
+            A[row][col] = (A[row][col] % MOD + MOD) % MOD;
+
+    int rank = 0;
+    for (int col = 0; col < n; ++col) {
+        if (is_extended && col == n-1) break;
+        int pivot = -1;
+        for (int row = rank; row < m; ++row) {
+            if (A[row][col] != 0) {
+                pivot = row;
+                break;
+            }
+        }
+        if (pivot == -1) continue;
+        swap(A[pivot], A[rank]);
+        auto inv = modinv(A[rank][col], MOD);
+        for (int col2 = 0; col2 < n; ++col2)
+            A[rank][col2] = A[rank][col2] * inv % MOD;
+        for (int row = 0; row < m; ++row) {
+            if (row != rank && A[row][col]) {
+                auto fac = A[row][col];
+                for (int col2 = 0; col2 < n; ++col2) {
+                    A[row][col2] -= A[rank][col2] * fac % MOD;
+                    if (A[row][col2] < 0) A[row][col2] += MOD;
+                }
+            }
+        }
+        ++rank;
+    }
+    return rank;
+}
+
+// 入力：連立一次方程式 Ax = b
+// return : Ax = b の解(mod. p)
+template<int MOD> int linear_equation(Matrix<MOD> A, vector<long long> b, vector<long long> &res) {
+    int m = A.size(), n = A[0].size();
+    Matrix<MOD> M(m, n + 1);
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) M[i][j] = A[i][j];
+        M[i][n] = b[i];
+    }
+    int rank = GaussJordan(M, true);
+
+    // check if it has no solution
+    for (int row = rank; row < m; ++row) if (M[row][n]) return -1;
+
+    // answer
+    res.assign(n, 0);
+    for (int i = 0; i < rank; ++i) res[i] = M[i][n];
+    return rank;
+}
+
+// ============================================================================================= //
+// ============================================================================================= //
+// ============================================================================================= //
+// ============================================================================================= //
+
+/*
+* Gauss-Jordan( mod. 2)
+*/
+
+const int MAX_ROW = 510; // to be set appropriately
+const int MAX_COL = 510; // to be set appropriately
+struct BitMatrix {
+    int H, W;
+    bitset<MAX_COL> val[MAX_ROW];
+    BitMatrix(int m = 1, int n = 1) : H(m), W(n) {}
+    inline bitset<MAX_COL>& operator [] (int i) {return val[i];}
+};
+
+// A : m x nの行列
+// return : Aのrank
+int GaussJordan(BitMatrix &A, bool is_extended = false) {
+    int rank = 0;
+    for (int col = 0; col < A.W; ++col) {
+        if (is_extended && col == A.W - 1) break;
+        int pivot = -1;
+        for (int row = rank; row < A.H; ++row) {
+            if (A[row][col]) {
+                pivot = row;
+                break;
+            }
+        }
+        if (pivot == -1) continue;
+        swap(A[pivot], A[rank]);
+        for (int row = 0; row < A.H; ++row) {
+            if (row != rank && A[row][col]) A[row] ^= A[rank];
+        }
+        ++rank;
+    }
+    return rank;
+}
+
+// 入力：連立一次方程式 Ax = b
+// return : Ax = b の解(mod. 2)
+int linear_equation(BitMatrix A, vector<int> b, vector<int> &res) {
+    int m = A.H, n = A.W;
+    BitMatrix M(m, n + 1);
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) M[i][j] = A[i][j];
+        M[i][n] = b[i];
+    }
+    int rank = GaussJordan(M, true);
+
+    // check if it has no solution
+    for (int row = rank; row < m; ++row) if (M[row][n]) return -1;
+
+    // answer
+    res.assign(n, 0);
+    for (int i = 0; i < rank; ++i) res[i] = M[i][n];
+    return rank;
+}
+
+// ============================================================================================= //
+// ============================================================================================= //
+// ============================================================================================= //
+// ============================================================================================= //

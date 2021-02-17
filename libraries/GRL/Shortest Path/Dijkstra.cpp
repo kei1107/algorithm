cf2ad8adbf4d43e221e21f7e63da6b27e940384c
keisuke kimura <32297845+kei1107@users.noreply.github.com>
2021-02-17 18:20:02 +0900

    clsss dijkstra : 改善

--- libraries/GRL/Shortest Path/Dijkstra.cpp
+++ libraries/GRL/Shortest Path/Dijkstra.cpp
@@ -1,56 +1,86 @@
-#include "bits/stdc++.h"
-using namespace  std;
-typedef long long ll;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
-#define INF 1<<30
-#define LINF 1LL<<60
-
-
+template<typename CType>
 class Dijkstra{
 public:
-    ll V;
-    ll root;
-    
     struct edge{
-        ll u;
-        ll v;
-        ll cost;
-        edge(){}
-        edge(ll u,ll v,ll c):u(u),v(v),cost(c){}
+        size_t u,v;
+        CType cost;
+        edge() {}
+        edge(size_t u,size_t v,CType c):u(u),v(v),cost(c) {}
     };
-    
+
+    size_t V;
+    size_t now_root;
+
     vector<vector<edge>> G;
-    vector<ll> dist;
-    
-    Dijkstra(ll V):V(V){
+    vector<CType> dist;
+
+    Dijkstra(size_t V):V(V){
         G.resize(V);
-        dist.assign(V, INF);
+        dist.resize(V);
+        init();
     }
-    void add_edge(ll u,ll v,ll cost, bool undirected = false){
+    void init(CType init_val = numeric_limits<CType>::max()){
+        fill(dist.begin(),dist.end(),init_val);
+    }
+
+    void add_directed_edge(size_t u,size_t v,CType cost){
         G[u].emplace_back(edge(u,v,cost));
-        if(undirected){
-            G[v].push_back(edge(v,u,cost));
-        }
     }
-    
-    void calc_dist(ll root){
-        this->root = root;
-        dist[root] = 0;
-        queue<pll> q; q.push({root,0});
-        while(!q.empty()){
-            auto p = q.front(); q.pop();
-            ll n = p.first;
-            ll now_d = p.second;
-            if(now_d > dist[n]) continue;
+    void add_undirected_biedge(size_t u,size_t v,CType cost){
+        add_directed_edge(u,v,cost);
+        add_directed_edge(v,u,cost);
+    }
+
+    void fit(size_t root){
+        now_root = root;
+        dist[now_root] = 0;
+
+        using Item = tuple<CType,size_t>;
+        priority_queue<Item,vector<Item>,greater<Item>> pq;
+        pq.emplace(0,now_root);
+
+        while(pq.size()){
+            auto [cost,n] = pq.top(); pq.pop();
+            if(cost > dist[n]) continue;
             for(auto e:G[n]){
                 if(dist[e.v] > dist[e.u] + e.cost){
                     dist[e.v] = dist[e.u] + e.cost;
-                    q.push({e.v,now_d + e.cost});
+                    pq.emplace(dist[e.v],e.v);
                 }
             }
         }
     }
-    
-    inline ll get_dist(ll n) const{ return dist[n];}
+    CType operator[](int i) { return dist[i]; }
 };
+
+
+// verified : https://atcoder.jp/contests/abc191/tasks/abc191_e
+template<class Type>
+Type solve(Type res = Type()){
+    int N,M; cin >> N >> M;
+    Dijkstra<int> dijkstra(N);
+
+    using edge_type = tuple<int,int,int>;
+    vector<edge_type> edges;
+    for(int i = 0; i < M;i++){
+        int A,B,C; cin >> A >> B >> C;
+        A--; B--;
+        dijkstra.add_directed_edge(A,B,C);
+        edges.emplace_back(A,B,C);
+    }
+
+    for(int n = 0; n < N; n++){
+        dijkstra.init();
+        dijkstra.fit(n);
+
+        int T = numeric_limits<int>::max();
+        for(auto [u,v,cost]: edges){
+            if(dijkstra[u] == numeric_limits<int>::max()) continue;
+            if(v != n) continue;
+            T = min(T,dijkstra[u] + cost);
+        }
+        if(T == numeric_limits<int>::max()) T = -1;
+        cout << T << endl;
+    }
+
+    return res;
